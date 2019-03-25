a50233bb613db887a18015f9499a38f0c9022724
illyr <david.regnier13@gmail.com>
2019-03-25 11:07:13 +0100

    Return ValidationResult

--- src/main/java/org/apache/commons/validator/routines/DomainValidatorWithTldSupplier.java
+++ src/main/java/org/apache/commons/validator/routines/DomainValidatorWithTldSupplier.java
@@ -3,10 +3,16 @@ package org.apache.commons.validator.routines;
 import static java.util.Locale.ENGLISH;
 import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
+import static java.util.Optional.ofNullable;
 import static org.apache.commons.validator.routines.DomainValidator.DOMAIN_NAME_REGEX;
 import static org.apache.commons.validator.routines.DomainValidator.MAX_DOMAIN_LENGTH;
 import static org.apache.commons.validator.routines.DomainValidator.chompLeadingDot;
 import static org.apache.commons.validator.routines.DomainValidator.unicodeToASCII;
+import static org.apache.commons.validator.routines.DomainValidatorWithTldSupplier.InvalidReason.FORMAT;
+import static org.apache.commons.validator.routines.DomainValidatorWithTldSupplier.InvalidReason.MAX_LENGTH_EXCEEDED;
+import static org.apache.commons.validator.routines.DomainValidatorWithTldSupplier.InvalidReason.TLD;
+
+import java.util.Optional;
 
 public enum DomainValidatorWithTldSupplier {
     ;
@@ -14,38 +20,67 @@ public enum DomainValidatorWithTldSupplier {
     private static final RegexValidator DOMAIN_REGEXP = new RegexValidator(DOMAIN_NAME_REGEX);
 
     /**
-     * Returns true if the specified <code>String</code> parses
-     * as a valid domain name with a recognized top-level domain.
+     * Returns a {@link ValidationResult} - result of the validation of the specified <code>String</code> parsing
+     * it and checking it is a valid domain name with a recognized top-level domain.
      * The parsing is case-insensitive.
      * <p>
      * Local domains are considered as invalid.
      *
      * @param tldSupplier the TLDs supplier to use
      * @param domain the parameter to check for domain name syntax
-     * @return true if the parameter is a valid domain name - local domains are considered as invalid
+     * @return the {@link ValidationResult}
      *
      * @throws NullPointerException if one of the given parameters is {@code null}
      */
-    public static boolean isDomainValid(final TldSupplier tldSupplier, final String domain) {
+    public static ValidationResult isDomainValid(final TldSupplier tldSupplier, final String domain) {
         requireNonNull(tldSupplier);
         requireNonNull(domain);
 
         final String asciiDomain = unicodeToASCII(domain);
 
         if (asciiDomain.length() > MAX_DOMAIN_LENGTH) {
-            return false;
+            return new ValidationResult(false, MAX_LENGTH_EXCEEDED);
         }
 
         final String[] groups = DOMAIN_REGEXP.match(asciiDomain);
 
         if(isNull(groups) || groups.length == 0) {
-            return false;
+            return new ValidationResult(false, FORMAT);
         }
 
-        return isValidTld(tldSupplier, groups[0]);
+        final boolean isValidTld = isValidTld(tldSupplier, groups[0]);
+        if(isValidTld) {
+            return new ValidationResult(true, null);
+        }
+        return new ValidationResult(false, TLD);
     }
 
     private static boolean isValidTld(final TldSupplier tldSupplier, final String tld) {
         return tldSupplier.get().contains(chompLeadingDot(unicodeToASCII(tld).toLowerCase(ENGLISH)));
+    }
+
+    public enum InvalidReason {
+        MAX_LENGTH_EXCEEDED,
+        FORMAT,
+        TLD
+    }
+
+    public static final class ValidationResult {
+
+        private final boolean isValid;
+        private final InvalidReason invalidReason;
+
+        private ValidationResult(final boolean isValid, final InvalidReason invalidReason) {
+            this.isValid = isValid;
+            this.invalidReason = invalidReason;
+        }
+
+        public boolean isValid() {
+            return isValid;
+        }
+
+        public Optional<InvalidReason> invalidReason() {
+            return ofNullable(invalidReason);
+        }
     }
 }
--- src/main/java/org/apache/commons/validator/routines/EmailValidatorWithTldSupplier.java
+++ src/main/java/org/apache/commons/validator/routines/EmailValidatorWithTldSupplier.java
@@ -1,9 +1,17 @@
 package org.apache.commons.validator.routines;
 
 import static java.util.Objects.requireNonNull;
+import static java.util.Optional.ofNullable;
+import static org.apache.commons.validator.routines.DomainValidatorWithTldSupplier.isDomainValid;
 import static org.apache.commons.validator.routines.EmailValidator.IP_DOMAIN_PATTERN;
 import static org.apache.commons.validator.routines.EmailValidator.isValidUser;
+import static org.apache.commons.validator.routines.EmailValidatorWithTldSupplier.InvalidReason.DOMAIN_MAX_LENGTH_EXCEEDED;
+import static org.apache.commons.validator.routines.EmailValidatorWithTldSupplier.InvalidReason.DOMAIN_TLD;
+import static org.apache.commons.validator.routines.EmailValidatorWithTldSupplier.InvalidReason.FORMAT;
+import static org.apache.commons.validator.routines.EmailValidatorWithTldSupplier.InvalidReason.INVALID_IP_DOMAIN_FORMAT;
+import static org.apache.commons.validator.routines.EmailValidatorWithTldSupplier.InvalidReason.USER_FORMAT;
 
+import java.util.Optional;
 import java.util.regex.Matcher;
 
 public enum EmailValidatorWithTldSupplier {
@@ -15,30 +23,88 @@ public enum EmailValidatorWithTldSupplier {
      * @param tldSupplier the TLDs supplier to use
      * @param email The value validation is being performed on.  A <code>null</code>
      *              value is considered invalid.
-     * @return true if the email address is valid.
+     * @return the {@link ValidationResult}.
      *
      * @throws NullPointerException if one of the given parameters is {@code null}
      */
-    public static boolean isValid(final TldSupplier tldSupplier, final String email) {
+    public static ValidationResult isValid(final TldSupplier tldSupplier, final String email) {
         requireNonNull(tldSupplier);
         requireNonNull(email);
 
         if (email.endsWith(".")) {
-            return false;
+            return new ValidationResult(false, FORMAT);
         }
 
         final Matcher emailMatcher = EmailValidator.EMAIL_PATTERN.matcher(email);
 
-        return emailMatcher.matches() &&
-               isValidUser(emailMatcher.group(1)) &&
-               isValidDomain(tldSupplier, emailMatcher.group(2));
+        if(!emailMatcher.matches()) {
+            return new ValidationResult(false, FORMAT);
+        }
+
+        if(!isValidUser(emailMatcher.group(1))) {
+            return new ValidationResult(false, USER_FORMAT);
+        }
+
+        return isValidDomain(tldSupplier, emailMatcher.group(2));
     }
 
-    protected static boolean isValidDomain(final TldSupplier tldSupplier, final String domain) {
+    protected static ValidationResult isValidDomain(final TldSupplier tldSupplier, final String domain) {
         final Matcher ipDomainMatcher = IP_DOMAIN_PATTERN.matcher(domain);
         if (ipDomainMatcher.matches()) {
-            return InetAddressValidator.getInstance().isValid(ipDomainMatcher.group(1));
+
+            if(!InetAddressValidator.getInstance().isValid(ipDomainMatcher.group(1))) {
+                return new ValidationResult(false, INVALID_IP_DOMAIN_FORMAT);
+            }
+            return new ValidationResult(true, null);
         }
-        return DomainValidatorWithTldSupplier.isDomainValid(tldSupplier, domain);
+
+        final DomainValidatorWithTldSupplier.ValidationResult domainResult = isDomainValid(tldSupplier, domain);
+        if(domainResult.isValid()) {
+            return new ValidationResult(true, null);
+        }
+        if(domainResult.invalidReason().isPresent()) {
+            return new ValidationResult(false, toEmailInvalidReason(domainResult.invalidReason().get()));
+        }
+        throw new IllegalStateException("Invalid result without reason!");
+    }
+
+    private static InvalidReason toEmailInvalidReason(final DomainValidatorWithTldSupplier.InvalidReason reason) {
+        switch(reason) {
+            case MAX_LENGTH_EXCEEDED:
+                return DOMAIN_MAX_LENGTH_EXCEEDED;
+            case FORMAT:
+                return FORMAT;
+            case TLD:
+                return DOMAIN_TLD;
+            default:
+                throw new UnsupportedOperationException("Unsupported reason: " + reason);
+        }
+    }
+
+    public enum InvalidReason {
+        FORMAT,
+        INVALID_IP_DOMAIN_FORMAT,
+        USER_FORMAT,
+        DOMAIN_MAX_LENGTH_EXCEEDED,
+        DOMAIN_TLD
+    }
+
+    public static final class ValidationResult {
+
+        private final boolean isValid;
+        private final InvalidReason invalidReason;
+
+        private ValidationResult(final boolean isValid, final InvalidReason invalidReason) {
+            this.isValid = isValid;
+            this.invalidReason = invalidReason;
+        }
+
+        public boolean isValid() {
+            return isValid;
+        }
+
+        public Optional<InvalidReason> invalidReason() {
+            return ofNullable(invalidReason);
+        }
     }
 }
